c25ca175936a3c4d54d72bdb28f519e7fabdeaf6
esteloabell <manitobaaa@gmail.com>
2021-08-24 13:27:14 -0500

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBJPw7enMgK3vKefXLMNhHudttz-PZFfGY",
+  authDomain: "instagram-native-832ff.firebaseapp.com",
+  projectId: "instagram-native-832ff",
+  storageBucket: "instagram-native-832ff.appspot.com",
+  messagingSenderId: "893600502254",
+  appId: "1:893600502254:web:5605d59a9a6b8ed4dbfeff",
+  measurementId: "G-4Q5H9PVQRK"
+};
