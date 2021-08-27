6e228d69514128650319244e3e9f0e8ecc672ebb
esteloabell <manitobaaa@gmail.com>
2021-08-27 12:38:11 -0500

    Create Profile.js

---
+++ components/main/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+
+export default function Profile() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Profile</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
