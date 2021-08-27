3d656929a7ca82cace852761ead60c40a389e7ef
esteloabell <manitobaaa@gmail.com>
2021-08-27 12:38:09 -0500

    Create Feed.js

---
+++ components/main/Feed.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, SafeAreaView} from 'react-native';
+
+export default function Feed() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Feed</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
